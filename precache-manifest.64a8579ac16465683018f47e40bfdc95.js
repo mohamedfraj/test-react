b8b866d5a106dce5131b1a5c37a76e5d8946096e
unknown <fraj.mohamed97@gmail.com>
2019-07-24 00:05:44 +0100

    Updates

---
+++ precache-manifest.64a8579ac16465683018f47e40bfdc95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1297d30123b0e4a7c6325128169823ce",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "2c6cab01bcf93ed197e0",
+    "url": "/test-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/test-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2c6cab01bcf93ed197e0",
+    "url": "/test-react/static/js/main.58fc98fd.chunk.js"
+  },
+  {
+    "revision": "31d3ffe18f2cb45b7bd6",
+    "url": "/test-react/static/js/runtime~main.bea1702e.js"
+  }
+]);
